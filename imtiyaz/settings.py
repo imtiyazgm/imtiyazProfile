d1a448a51bf9ae66e27a293c7d2430ee50f482af
imtiyaz ahmed <imtiyazgm@yahoo.com>
2022-03-31 12:36:06 +0400

    tweaked app for heroku

--- imtiyaz/settings.py
+++ imtiyaz/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os 
+import django_heroku
+import dj_database_url
+from decouple import config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
